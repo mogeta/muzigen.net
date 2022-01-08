3c4bce3eef2595896344a2ebdca7e737b31ffc4d
mogeta <muzigen@gmail.com>
2022-01-08 23:07:32 +0900

    meta

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -4,6 +4,8 @@ import {doc, Firestore, getDoc} from '@angular/fire/firestore';
 import firebase from 'firebase/compat';
 import Timestamp = firebase.firestore.Timestamp;
 import {IdleMonitorService} from "@scullyio/ng-lib";
+import {Meta, Title} from "@angular/platform-browser";
+import {environment} from "../../../environments/environment";
 
 export class Item {
   id: string;
@@ -30,7 +32,11 @@ export class ArticleComponent implements OnInit {
   storageUrl = 'https://firebasestorage.googleapis.com/v0/b/muzigen-net.appspot.com/o/markdown%2F';
   query = '?alt=media';
 
-  constructor(private fs: Firestore, private route: ActivatedRoute, private ims: IdleMonitorService) {
+  constructor(private fs: Firestore,
+              private route: ActivatedRoute,
+              private title: Title,
+              private ims: IdleMonitorService,
+              private meta: Meta) {
   }
 
   async ngOnInit(): Promise<void> {
@@ -39,7 +45,20 @@ export class ArticleComponent implements OnInit {
     const resut = await getDoc(ref);
     this.article = resut.data() as Item;
     this.article.toLocaleString = this.article.created_date.toDate().toLocaleString();
+    this.updateMeta(this.article.title);
     await this.ims.fireManualMyAppReadyEvent();
   }
 
+  private updateMeta(title: string): void {
+    this.title.setTitle(title);
+    console.log();
+    // this.meta.updateTag({ name: 'description', content: desc })
+    // this.meta.updateTag({ name: 'keywords', content: keywords })
+    // this.meta.updateTag({ name: 'twitter:card', content: twittercard })
+    // this.meta.updateTag({ name: 'twitter:site', content: twittersite })
+    this.meta.updateTag({ property: 'og:url', content: `https://${environment.domain}/${this.route.snapshot.url.join('/')}`});
+    // this.meta.updateTag({ property: 'og:title', content: title })
+    // this.meta.updateTag({ property: 'og:description', content: desc })
+    // this.meta.updateTag({ property: 'og:image', content: twitterimage })
+  }
 }
