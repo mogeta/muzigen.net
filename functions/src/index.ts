d3615169095c4da3ae83b9d76318b82ac3175188
mogeta <muzigen@gmail.com>
2022-01-10 22:21:51 +0900

    add blog list function

---
+++ functions/src/index.ts
@@ -0,0 +1,30 @@
+import * as functions from "firebase-functions";
+
+// The Firebase Admin SDK to access Firestore.
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+
+// export class Blog {
+//   constructor(
+//     public id: string,
+//       // public name: string,
+//       // public price: number,
+//   ) {
+//   }
+// }
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const blogs = functions.https.onRequest(async (request, response) => {
+  // functions.logger.info("Hello logs!", {structuredData: true});
+  const querySnapshot = await admin.firestore().collection("blog_contents")
+      .select("id")
+      .orderBy("update_date", "desc").get();
+  const records = querySnapshot.docs.map((elem) => elem.data());
+
+  // await admin.firestore().collection('messages');
+  response.send(JSON.stringify(records));
+});
